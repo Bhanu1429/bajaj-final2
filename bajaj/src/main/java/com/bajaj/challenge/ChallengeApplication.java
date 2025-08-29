843b22328ae93dd03eea77f5ac5632d017be1a15
Bhanu1429 <135256216+Bhanu1429@users.noreply.github.com>
2025-08-29 11:10:51 +0530

    Add files via upload

---
+++ bajaj/src/main/java/com/bajaj/challenge/ChallengeApplication.java
@@ -0,0 +1,3 @@
+private static final String APPLICANT_NAME = "Bhanu sharma";
+private static final String APPLICANT_REG_NO = "22bit0247"; // Use your actual RegNo
+private static final String APPLICANT_EMAIL = "bhanu.sharma2022@vitstudent.ac.in";
